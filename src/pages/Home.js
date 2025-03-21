dad7546a2ca4c6199cb0654bd8a1dab49a8f36b6
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-21 19:43:24 -0300

    Commit aula 20/03

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,31 @@
-import { StyleSheet, Text, View, ImageBackground, TouchableOpacity } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { ImageBackground, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-export default function Home({navigation}) {
+export default function Home({ navigation }) {
   return (
-    <ImageBackground 
-        source={require('../../assets/HomePage.png')}
-        style={styles.container}
+    <ImageBackground
+      source={require('../../assets/HomePage.png')}
+      style={styles.container}
     >
-        <Text style={styles.textoPrincipal}>Pesquisar por...</Text>
-        <View style={styles.buttonsContainer}>
-            <TouchableOpacity 
-                style={styles.btn}
-                // Estruturando um objeto na função que passará um valor para a outra tela
-                onPress={() => navigation.navigate('Result', {escolha: 'gifs'})}
-            >
-                <Text style={styles.btnText}>GIFS</Text>
-            </TouchableOpacity>
+      <Text style={styles.textoPrincipal}>Pesquisar por...</Text>
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity
+          style={styles.btn}
+          // Estruturando um objeto na função que passará um valor para a outra tela
+          onPress={() => navigation.navigate('Result', { escolha: 'gifs' })}
+        >
+          <Text style={styles.btnText}>GIFS</Text>
+        </TouchableOpacity>
 
-            <TouchableOpacity 
-                style={styles.btn}
-                onPress={() => navigation.navigate('Result', {escolha: 'stickers'})}
-            >
-                <Text style={styles.btnText}>STICKERS</Text>
-            </TouchableOpacity>
-        </View>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => navigation.navigate('Result', { escolha: 'stickers' })}
+        >
+          <Text style={styles.btnText}>STICKERS</Text>
+        </TouchableOpacity>
+      </View>
+
+      <StatusBar style='light' />
     </ImageBackground>
   );
 }
@@ -43,7 +46,7 @@ const styles = StyleSheet.create({
     paddingBlock: 32,
     flexDirection: 'row',
     gap: 20,
-  }, 
+  },
   btn: {
     paddingBlock: 8,
     paddingInline: 16,
--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,29 +1,84 @@
-import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { ImageBackground, StyleSheet, Text } from 'react-native';
+import API_KEY from '../API_KEY';
+import axios from 'axios';
 
-export default function Result({route}){
-    const escolha = route.params.escolha;
-    const link = `api.giphy.com/v1/${escolha}/search`;
-    console.log(link)
-    return (
-        <ImageBackground
-            source={require('../../assets/BG.png')}
-            style={styles.container}
+
+export default function Result({ route }) {
+  const escolha = route.params.escolha;
+  const [textoPesquisa, setTextoPesquisa] = useState("")
+  const link = `api.giphy.com/v1/${escolha}/search?api_key=${API_KEY}&q=${textoPesquisa ? textoPesquisa : "?"}&limit=10&offset=0&rating=g&lang=pt&bundle=messaging_noclips`;
+  const [gifUrls, setGifUrls] = useState([]);
+
+  const pesquisar = async () => {
+    try{
+      const response = await axios.get(link);
+      const urls = response.data.data.map(gif => gif.images.original.url);
+      setGifUrls(urls);
+    }catch(erro){
+      console.error('Erro ao busca GIF: ', erro);
+    }
+  }
+
+  return (
+    <ImageBackground
+      source={require('../../assets/BG.png')}
+      style={styles.background}
+    >
+      <SafeAreaView style={styles.container}>
+        <View style={{width:'100%', flexDirection:'row', justifyContent:'space-between', padding:5}}>
+          <Ionicons name='chevron-back' size={40} color='white' onPress={ () => { navigation.goBack() } } />
+          <TextInput 
+            placeholder='Digite sua pesquisa' 
+            style={styles.input} 
+            autoCapitalize='none'
+            value={textoPesquisa}
+            onChangeText={setTextoPesquisa}
+            returnKeyType="done"
+          />
+          <Ionicons name='search' size={40} color='white' onPress={pesquisar}/>
+        </View>
+
+        <ScrollView 
+          contentContainerStyle={styles.scrollContainer} 
+          showsVerticalScrollIndicator={false}
         >
-            <Text style={styles.textoPrincipal}>{link}</Text>
-        </ImageBackground>
-    );
+          {gifUrls.map((url, index) => (
+            <Image
+              key={index}
+              source={{ uri: url }}
+              style={styles.gif}
+              resizeMode='contain'
+            />
+          ))}
+
+          <StatusBar style="light" />
+        </ScrollView>
+      </SafeAreaView>
+    </ImageBackground>
+  );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  textoPrincipal: {
-    color: '#fff',
-    fontSize: 25,
-    fontWeight: 'bold'
+  scrollContainer: {
+    alignItems: 'center',
   },
+  gif: {
+    width: 300,
+    height: 300,
+    marginTop: 20,
+  },
+  input: {
+    backgroundColor: 'white',
+    width: '70%',
+    borderRadius: 15
+  }
 });
